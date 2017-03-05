87131946d2d37b8f96c771eefe3ccbfd109edcaa
Anubhav Singh <Anubhav Singh>
2017-03-05 14:10:12 +0000

    ngModel to slider added

--- app/components/some.component.ts
+++ app/components/some.component.ts
@@ -6,4 +6,19 @@ import { Component } from '@angular/core';
   templateUrl: `some.component.html`,
   styleUrls: ['some.component.css']
 })
-export class SomeComponent  { name = 'SomeComponent'; }
+export class SomeComponent  { 
+  name = 'SomeComponent';
+  slider1Value: number;
+  slider2Value: number;
+  savedAmount = 1;
+
+  constructor() {
+    this.slider1Value=1;
+    this.slider2Value=1;
+    this.savedAmount=1;
+  }
+
+  onChange() {
+    this.savedAmount = this.slider1Value*this.slider2Value*10;
+  }
+}
